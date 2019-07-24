c019c0ca4dbb0d4a749b0c9cb0c8cf80f37307e6
Raghad Alafeef <raghadalafeef@gmail.com>
2019-07-24 20:25:36 +0300

    Initial plan for code

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+
+#	CREATE 2D ARRAY AS INITIAL GRID
+grid = [[ random.randint(0,1) for x in range(0,10)] for y in range(0,10)]
+
+#print(grid)
+
+automate(grid)
+
+
+#	UPDATE TO NEW GRID
+def automate(originalGrid):
+	
+
+
+	pass
+
+
+#	RULE FUNCTION
+def calculate(originalGrid):
+
+
+
